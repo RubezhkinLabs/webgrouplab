944b5f80a184ed5b24d8e77b36010deabfe703b7
Rubezhkin <pavelned03@gmail.com>
2025-03-18 23:42:29 +0300

    Добавлен выбор системы счисления

--- index.js
+++ index.js
@@ -6,25 +6,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const okButton = document.getElementById("checkButton");
   const resetButton = document.getElementById("resetButton");
   const numberButtons = document.querySelectorAll(".numberButton");
+  const numberSystem = document.getElementById("numberSystem");
 
-  let num1, num2, answer, operation;
+  let num1, num2, answer, operation, numSys;
 
   function generateProblem() {
-    num1 = Math.floor(Math.random() * 64);
-    num2 = Math.floor(Math.random() * 64);
+    numSys = parseInt(numberSystem.value);
+    console.log(numSys);
+    num1 = Math.floor(Math.random() * 256);
+    num2 = Math.floor(Math.random() * 256);
     operation = Math.random() < 0.5 ? "+" : "-";
     if (operation === "-" && num1 < num2) {
       [num1, num2] = [num2, num1];
     }
-    num1Input.value = num1.toString(8);
+    num1Input.value = num1.toString(numSys);
     operationInput.value = operation;
-    num2Input.value = num2.toString(8);
+    num2Input.value = num2.toString(numSys);
     resultInput.value = "";
     feedbackInput.value = "";
   }
 
   function checkAnswer() {
-    const userAnswer = parseInt(resultInput.value, 8);
+    const userAnswer = parseInt(resultInput.value, numSys);
     if (operation === "+") {
       answer = num1 + num2;
     } else {
@@ -44,7 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      resultInput.value += button.textContent;
+      let num = parseInt(button.textContent);
+      if (num < numSys) {
+        resultInput.value += button.textContent;
+      }
     });
   });
 
